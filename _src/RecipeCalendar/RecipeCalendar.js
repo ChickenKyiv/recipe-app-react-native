e8618152561091a711339ed725f990cad36d296d
Arthur Tkachenko <atherdon@gmail.com>
2018-05-15 23:20:54 +0300

    xxxx

--- _src/RecipeCalendar/RecipeCalendar.js
+++ _src/RecipeCalendar/RecipeCalendar.js
@@ -9,7 +9,27 @@ import data  from '../../data/json-db-sample/'
 
 const RecipeCalendar = (props) => {
   return (
-
+    <Calendar>
+      <Title text="Recipes Calendar" />
+      <Days>
+        {/* this is a loop */}
+        <Day>
+          <Icon name="nutrition"></Icon>
+          {day.name}
+        </Day>
+        <Body>
+          <RecipeTitle></RecipeTitle>
+          {/*
+             Letter is just a link to main recipe.
+             Like first recipe is weekly menu list is came with `A`
+             Second letter came with `B`
+             We don't need to have a custom tag for recipe letter.
+             this just for a sample purposes.
+           */}
+          <RecipeLetter></RecipeLetter>
+        </Body>
+      </Days>
+    </Calendar>
   );
 };
 
--- _src/Tabs/tabs.js
+++ _src/Tabs/tabs.js
@@ -9,7 +9,18 @@ import React from 'react';
 
 const Tabs = (props) => {
   return (
-    
+    {/* maybe it'll be anothe way to do menu? */}
+    <Tabs>
+      <Item title="Weekly Menu"
+            icon="list-outline" link="/weekly-menu">
+      </Item>
+      <Item title="Grocery Settings"
+            icon="heart" link="/grocery-settings">
+      </Item>
+      <Item title="Grocery List"
+            icon="checkbox-outline" link="/grocery-list">
+      </Item>
+    </Tabs>
   );
 };
 
